9f9e2fcfdff1f824bdeb5ab05f61946e4c7eb424
phael-tavares <raphael_tavares2000@hotmail.com>
2021-12-15 01:30:19 -0300

    Initial Commit - Minimal API with 2 endpoints (retrieve tasks and add tasks) using in memory entity framework database

---
+++ Program.cs
@@ -0,0 +1,45 @@
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("TasksDB"));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapGet("/tasks", async (AppDbContext db) => await db.Tasks.ToListAsync());
+
+app.MapPost("/tasks", async (Task task, AppDbContext db) =>
+{
+    db.Tasks.Add(task);
+    await db.SaveChangesAsync();
+    return Results.Created($"/tasks/{task.Id}", task);
+});
+
+
+app.Run();
+
+class Task
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public bool IsDone{ get; set; }
+}
+
+class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Task> Tasks=> Set<Task>();
+}
