aab3ae7f56d29d55eebfdfa9e97922c14f7c9cf4
phael-tavares <raphael_tavares2000@hotmail.com>
2021-12-18 15:58:09 -0300

    Add update, delete, getById and getDone endpoints, as well as setting the IsDone field as nullable

--- Program.cs
+++ Program.cs
@@ -20,11 +20,39 @@ if (app.Environment.IsDevelopment())
 
 app.MapGet("/tasks", async (AppDbContext db) => await db.Tasks.ToListAsync());
 
+app.MapGet("/tasks/{id}", async (int id, AppDbContext db) => await db.Tasks.FindAsync(id) is Task task ? Results.Ok(task) : Results.NotFound());
+
+app.MapGet("tasks/done", async (AppDbContext db) => await db.Tasks.Where(task => task.IsDone ?? false).ToListAsync());
+
 app.MapPost("/tasks", async (Task task, AppDbContext db) =>
 {
     db.Tasks.Add(task);
     await db.SaveChangesAsync();
     return Results.Created($"/tasks/{task.Id}", task);
+});
+
+app.MapPut("/tasks{id}", async (int id, Task inputTask, AppDbContext db) =>
+{
+    var task = await db.Tasks.FindAsync(id);
+
+    if (task is null) return Results.NotFound();
+
+    task.Name = inputTask?.Name ?? task.Name;
+    task.IsDone = inputTask?.IsDone ?? task.IsDone;
+
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
+
+app.MapDelete("/tasks/{id}", async (int id, AppDbContext db) =>
+{
+    if(await db.Tasks.FindAsync(id) is Task task)
+    {
+        db.Tasks.Remove(task);
+        await db.SaveChangesAsync();
+        return Results.Ok(task);
+    }
+    return Results.NotFound();
 });
 
 
@@ -34,7 +62,7 @@ class Task
 {
     public int Id { get; set; }
     public string? Name { get; set; }
-    public bool IsDone{ get; set; }
+    public bool? IsDone{ get; set; }
 }
 
 class AppDbContext : DbContext
